c32c073c26b9f77172f08dfa021f69a4ed1e912f
Natanael Arndt <arndtn@gmail.com>
2025-03-27 10:56:07 +0100

    Add tests

---
+++ tests/test_cli.py
@@ -0,0 +1,28 @@
+import kisumu
+from click.testing import CliRunner
+from kisumu import cli
+import importlib.resources
+import simpsons_rdf
+
+
+def test_build():
+    runner = CliRunner()
+    with importlib.resources.path(
+        simpsons_rdf, "simpsons.ttl"
+    ) as graph_path, importlib.resources.path(
+        kisumu, "example/homer.jinja"
+    ) as template_path:
+        result = runner.invoke(
+            cli,
+            [
+                "build",
+                "--template",
+                str(template_path),
+                "--graph",
+                str(graph_path),
+                "--resource",
+                str(simpsons_rdf.SIM.Homer.n3()),
+            ],
+        )
+        assert result.exit_code == 0
+        assert result.output == "Hello, Homer Simpson!"
---
+++ tests/test_template.py
@@ -0,0 +1,26 @@
+import kisumu
+from kisumu import render, stream
+import importlib.resources
+from simpsons_rdf import simpsons, SIM
+from io import StringIO
+
+
+def test_graph():
+    from rdflib.resource import Resource
+
+    print(simpsons)
+    print(SIM.Homer)
+    Resource(simpsons, SIM.Homer)
+
+
+def test_render():
+    template = importlib.resources.read_text(kisumu, "example/homer.jinja")
+    output = render(template=template, graph=simpsons.graph, resource=SIM.Homer)
+    assert output == "Hello, Homer Simpson!"
+
+
+def test_stream():
+    with StringIO() as output:
+        template = importlib.resources.read_text(kisumu, "example/homer.jinja")
+        stream(template=template, graph=simpsons.graph, resource=SIM.Homer).dump(output)
+        assert output.getvalue() == "Hello, Homer Simpson!"
